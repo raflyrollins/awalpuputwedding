6393dc42bdbddf06ef93e5c6b0feb03bad879c82
biraluvfly12 <biraluvfly@gmail.com>
2026-06-26 02:45:33 +0800

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/awalpuputwedding/",
+  // base: "/awalpuputwedding/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
